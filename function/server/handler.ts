e9bd84da98485dbd6c74a1be372ade8b97a0c947
Daniel Gary <daniel@gatewayapps.com>
2021-07-29 00:52:49 -0500

    more likely to work

--- function/server/handler.ts
+++ function/server/handler.ts
@@ -5,28 +5,8 @@ import {
 } from "@remix-run/node";
 
 import { Context } from "@azure/functions";
-import zlib from "zlib";
-import path from "path";
-import fs from "fs";
 
 installGlobals();
-
-import { resolve } from "path";
-import { readdir } from "fs/promises";
-
-const PUBLIC_CACHE_SECONDS = 60 * 60 * 24 * 7;
-
-async function* getFiles(dir: string): AsyncGenerator<string> {
-  const entries = await readdir(dir, { withFileTypes: true });
-  for (const entry of entries) {
-    const res = resolve(dir, entry.name);
-    if (entry.isDirectory()) {
-      yield* getFiles(res);
-    } else {
-      yield res;
-    }
-  }
-}
 
 function createRemixRequest(req) {
   let body;
@@ -40,7 +20,9 @@ function createRemixRequest(req) {
     req.headers.cookie = req.cookies.join(";");
   }
 
-  return new Request(req.url, {
+  const originalUrl = req.headers["x-ms-original-url"];
+  console.log(req.url);
+  return new Request(originalUrl, {
     method: req.httpMethod,
     headers: req.headers,
     body,
@@ -49,40 +31,16 @@ function createRemixRequest(req) {
 
 export function createRequestHandler({ build, mode = process.env.NODE_ENV }) {
   const handleRequest = createNodeRequestHandler(build, mode);
-  const ROOT_DIR = path.resolve(__dirname, "../../../public");
-  const PUBLIC_FILES: { [key: string]: string } = {};
-
-  (async () => {
-    for await (const f of getFiles(ROOT_DIR)) {
-      const fileName = path.relative(ROOT_DIR, f).replace(/\\/g, "/");
-      PUBLIC_FILES[`${fileName}`] = f;
-    }
-  })();
 
   return async (context: Context) => {
-    const pathName = new URL(context.req!.url).pathname.substr(1);
-    if (PUBLIC_FILES[pathName]) {
-      const buffer = zlib.gzipSync(fs.readFileSync(PUBLIC_FILES[pathName]));
+    console.log(context);
+    const response = await handleRequest(createRemixRequest(context.req));
+    const text = await response.text();
 
-      context.res = {
-        status: 200,
-        isRaw: true,
-        headers: {
-          "cache-control": `max-age=${PUBLIC_CACHE_SECONDS}`,
-          "content-encoding": "gzip",
-          "content-length": buffer.length,
-        },
-        body: buffer,
-      };
-    } else {
-      const response = await handleRequest(createRemixRequest(context.req));
-      const text = await response.text();
-
-      context.res = {
-        status: response.status,
-        headers: Object.fromEntries(response.headers),
-        body: text,
-      };
-    }
+    context.res = {
+      status: response.status,
+      headers: Object.fromEntries(response.headers),
+      body: text,
+    };
   };
 }
--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "azure/functions/server/build",
-  devServerPort: 7071
+  serverBuildDirectory: "function/server/build",
+  devServerPort: 7071,
 };
